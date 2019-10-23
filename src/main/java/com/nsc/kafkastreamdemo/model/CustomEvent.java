dc5ca0552308fbee3497a59f650e9daf6d72b9f3
Pratap A K <pratap.ak@ge.com>
2019-10-23 15:04:27 +0530

    feature: implemented aggregate

--- src/main/java/com/nsc/kafkastreamdemo/model/CustomEvent.java
+++ src/main/java/com/nsc/kafkastreamdemo/model/CustomEvent.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -13,4 +16,5 @@ public class CustomEvent {
     String name;
     Long value;
     String place;
+    List<Event> eventList = new ArrayList<>();
 }
--- src/main/java/com/nsc/kafkastreamdemo/receiver/MessageReceiver.java
+++ src/main/java/com/nsc/kafkastreamdemo/receiver/MessageReceiver.java
@@ -4,12 +4,15 @@ import com.nsc.kafkastreamdemo.model.CustomEvent;
 import com.nsc.kafkastreamdemo.model.Event;
 import com.nsc.kafkastreamdemo.sink.EventSink;
 import com.nsc.kafkastreamdemo.source.EventSource;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.kafka.support.serializer.JsonSerde;
 import org.springframework.messaging.handler.annotation.SendTo;
+
+import java.util.ArrayList;
 
 @EnableBinding(EventSink.class)
 public class MessageReceiver {
@@ -19,20 +22,35 @@ public class MessageReceiver {
     public KStream<String, CustomEvent> process(KStream<String, Event> eventKStream) {
         // return eventKStream.map((key, value) -> KeyValue.pair(key, new CustomEvent(value.getName(), value.getValue(), "Bengaluru")));
 
-        return eventKStream
+        /*return eventKStream
                 .filter((key, value) -> value.getValue() > 10)
                 .groupByKey()
                 .windowedBy(TimeWindows.of(15000))
                 .count()
                 .toStream((key, value) -> key.key())
-                .map((key, value) -> KeyValue.pair(key, new CustomEvent(key, value, "Pratap")));
+                .map((key, value) -> KeyValue.pair(key, new CustomEvent(key, value, "Pratap")));*/
+
+        JsonSerde<ArrayList<Event>> listOfEventJsonSerde = new JsonSerde<>(ArrayList.class);
+
+        return eventKStream
+                .groupByKey()
+                .windowedBy(TimeWindows.of(20000))
+                .aggregate(
+                        ArrayList::new,
+                        (key, event, eventList) -> {
+                            eventList.add(event);
+                            return eventList;
+                        },
+                        Materialized.with(Serdes.String(), listOfEventJsonSerde))
+                .toStream((key, value) -> key.key())
+                .map((key, value) -> KeyValue.pair(key, new CustomEvent(key, 10L, "Pratap", value)));
     }
 
     @StreamListener(target = EventSink.CUSTOM_EVENT_INPUT)
     public void processSecondLevel(KStream<String, CustomEvent> eventKStream) {
         eventKStream.foreach((key, value) -> {
-            System.out.println("----------- After Process ----------");
-            System.out.println(String.format("Patient crossed threshold %d times in the last 15 seconds", value.getValue()));
+            System.out.println("key = " + key);
+            System.out.println("value = " + value.getEventList());
         });
     }
 }
